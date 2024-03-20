9f6c0f2a837e6b2bdc910617c3a3605c2d5e2e63
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2024-03-20 11:49:27 +0530

    welcome page

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from api.blog import router as blog_router
 
 app = FastAPI()
 
+# Welcome Page Route
+@app.get("/", response_class=HTMLResponse)
+async def welcome_page():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Welcome to My FastAPI App</title>
+    </head>
+    <body>
+        <h1>Welcome!</h1>
+        <p>This is a FastAPI application.</p>
+        <p>Check out our <a href="/docs">API Documentation</a>.</p>
+    </body>
+    </html>
+    """
+
 app.include_router(auth_router, prefix="", tags=["auth"])
 app.include_router(blog_router, prefix="", tags=["blogs"])
 
